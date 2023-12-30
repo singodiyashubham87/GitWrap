e6c9bb294f9aebe74b8d3ffd2e682f86846cad3a
singodiyashubham87 <singodiyashubham87@gmail.com>
2023-12-30 18:45:02 +0530

    remove canvas border

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -7,7 +7,7 @@ const Canvas = () => {
   }, []);
 
   return (
-    <div className="fixed top-0 bottom-0 right-0 left-0 overflow-hidden border-2 border-black">
+    <div className="fixed top-0 bottom-0 right-0 left-0 overflow-hidden">
       <canvas id="myCanvas"></canvas>
     </div>
   );
