a27125d9ad5bb96ff1ace50046f058949f7a7f32
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-02-09 23:33:12 +0530

    chore: refactor homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,72 +1,34 @@
-// External libraries
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
-// Components
 import Modal from "../components/Modal";
 import Loader from "../components/Loader";
 import Canvas from "../components/Canvas";
 import Cursor from "../components/Cursor";
-
-// Images
 import logo from "../assets/images/logo.png";
 import octocatImage from "../assets/images/octocat.png";
-
-// Icons
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
-
-// Utilities
 import validateGithubUsername from "../utils/validateGithubUsername";
 
 const Homepage = () => {
-  const navigateTo = useNavigate(); // Navigation control
+  const navigateTo = useNavigate();
   const [showModal, setShowModal] = useState(false);
   const [ghUsername, setGhUsername] = useState("");
-  const [loader, setLoader] = useState(false); //loader variable
+  const [loader, setLoader] = useState(false);
 
-  // to clear localStorage data
-  useEffect(() => {
-    localStorage.clear();
-  }, []);
+  useEffect(() => localStorage.clear(), []);
 
-  // Update Github Username value when user type in the input box
-  const handleInputChange = (e) => {
-    setGhUsername(e.target.value);
-  };
+  const handleInputChange = (e) => setGhUsername(e.target.value);
 
   const handleSubmit = async (e) => {
-    if (e.type === "click") {
+    if (e.type === "click" || e.key === "Enter") {
       e.preventDefault();
-    } else {
-      if (e.key !== "Enter") {
-        return;
-      }
-    }
-
-    // if input is empty then show modal popup
-    if (!ghUsername) {
-      openModal();
-      return;
-    }
-
-    showLoader();
-    const isValidUsername = await validateGithubUsername(ghUsername);
-    if (!isValidUsername) {
-      hideLoader();
-      openModal();
-    } else {
-      hideLoader();
-      navigateTo("/stats");
+      if (!ghUsername) return setShowModal(true);
+      setLoader(true);
+      const isValidUsername = await validateGithubUsername(ghUsername);
+      setLoader(false);
+      isValidUsername ? navigateTo("/stats") : setShowModal(true);
     }
   };
-
-  // Show & Hide Loader component
-  const showLoader = () => setLoader(true);
-  const hideLoader = () => setLoader(false);
-
-  // Show & Hide Model
-  const openModal = () => setShowModal(true);
-  const closeModal = () => setShowModal(false);
 
   return (
     <>
@@ -74,25 +36,17 @@ const Homepage = () => {
         <Modal
           alert="Alert!"
           alertError="Enter a valid GitHub username!"
-          closeModal={closeModal}
+          closeModal={() => setShowModal(false)}
         />
       )}
       {loader && <Loader />}
       <Canvas />
       <Cursor />
       <div className="mainContainer min-h-[100dvh] w-[100%] relative flex flex-col justify-center items-center">
-        <img
-          src={logo}
-          alt="logo"
-          className="absolute w-[10rem] vvsm:w-[15rem] gsm:w-[20rem] top-[0.5rem] left-[0.5rem] gsm:top-[1rem] gsm:left-[1rem]"
-        />
+        <img src={logo} alt="logo" className="absolute w-[10rem] vvsm:w-[15rem] gsm:w-[20rem] top-[0.5rem] left-[0.5rem] gsm:top-[1rem] gsm:left-[1rem]" />
         <div className="contentContainer flex flex-col justify-center items-center gap-[1rem] gsm:gap-[2rem]">
-          <img
-            src={octocatImage}
-            alt="octocat"
-            className="w-[10rem] vsm:w-[12rem] gsm:w-[15rem]"
-          />
-          <div className="inputContainer flex flex-col gsm:flex-row gap-[1rem] justify-center items-center gsm:items-stretch ">
+          <img src={octocatImage} alt="octocat" className="w-[10rem] vsm:w-[12rem] gsm:w-[15rem]" />
+          <div className="inputContainer flex flex-col gsm:flex-row gap-[1rem] justify-center items-center gsm:items-stretch">
             <input
               type="text"
               className="inputBox bg-lightBlue px-[0.5rem] pl-[1rem] vvsm:px-[1rem] vvsm:pl-[1.5rem] vsm:px-[1.5rem] vsm:pl-[2rem] rounded-[5rem] text-[#505957] text-[1rem] vvsm:text-[1.2rem] vsm:text-[1.4rem] gsm:text-[1.7rem] font-primary font-semibold outline-none"
